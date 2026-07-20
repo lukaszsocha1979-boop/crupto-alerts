5d488189860274499c39b59384ebf28171417360
Mardog <lukaszsocha1979@gmail.com>
2026-07-20 21:51:12 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+KEYWORDS = {
+    "BTC": ["bitcoin", "btc"],
+    "ZEUS": ["zeus", "zeus network", "zbtc"],
+    "JUP": ["jupiter", "jup"],
+    "PYTH": ["pyth", "pyth network"],
+    "WORMHOLE": ["wormhole"],
+    "MEW": ["cat in a dogs world", "mew"],
+    "BOME": ["book of meme", "bome"]
+}
+
+HIGH_PRIORITY = [
+    "partnership",
+    "integration",
+    "listing",
+    "binance",
+    "coinbase",
+    "kraken",
+    "security",
+    "hack",
+    "exploit",
+    "bridge",
+    "mainnet",
+    "upgrade",
+    "token unlock"
+]
