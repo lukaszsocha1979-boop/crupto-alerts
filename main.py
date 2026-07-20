033002550c57e4891c06b857288061bc6ae64154
Mardog <lukaszsocha1979@gmail.com>
2026-07-20 21:56:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import os
 import requests
 
+from news_sources import get_news
+from filters import analyze
+
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-response = requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": "Test z GitHub Actions"
-    }
-)
+def send(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-print("Status:", response.status_code)
-print("Odpowiedź:", response.text)
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        }
+    )
+
+
+news = get_news()
+
+alerts = analyze(news)
+
+if not alerts:
+    print("Brak nowych alertów.")
+else:
+
+    for a in alerts:
+
+        message = (
+            f"{a['level']}\n\n"
+            f"{a['project']}\n\n"
+            f"{a['title']}\n\n"
+            f"{a['link']}"
+        )
+
+        send(message)
+
+        print(message)
