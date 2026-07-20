6a37a7ceb4ec675c45a8168eb4679fc35dde3c97
Mardog <lukaszsocha1979@gmail.com>
2026-07-20 21:31:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import requests
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-message = "✅ Crypto Alerts działa!\n\nTo jest wiadomość testowa z GitHub Actions."
-
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": message
-})
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "Test z GitHub Actions"
+    }
+)
 
-print("Test zakończony.")
+print("Status:", response.status_code)
+print("Odpowiedź:", response.text)
