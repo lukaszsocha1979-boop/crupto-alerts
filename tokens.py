e605e43b035a00b8afcf6dae263507572cde10d5
Mardog <lukaszsocha1979@gmail.com>
2026-07-21 13:15:22 +0200

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,6 +1,4 @@
-TOKENS = {
-    "BTC": {
-        "type": "coingecko",
-        "id": "bitcoin",
-    },
-}
+"ZEUS": {
+    "type": "solana",
+    "mint": "ZEUS1aR7aX8DFFJf5QjWj2ftDDdNTroMNGo8YoQm3Gq"
+},
