52d8854cf658f18f122665deadcb7ed4482460b5
Mardog <lukaszsocha1979@gmail.com>
2026-07-21 13:01:08 +0200

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,41 +1,6 @@
 TOKENS = {
     "BTC": {
         "type": "coingecko",
-        "id": "bitcoin"
+        "id": "bitcoin",
     },
-
-    "ZEUS": {
-        "type": "dexscreener",
-        "symbol": "ZEUS"
-    },
-
-    "JUP": {
-        "type": "dexscreener",
-        "symbol": "JUP"
-    },
-
-    "PYTH": {
-        "type": "dexscreener",
-        "symbol": "PYTH"
-    },
-
-    "W": {
-        "type": "dexscreener",
-        "symbol": "W"
-    },
-
-    "MEW": {
-        "type": "dexscreener",
-        "symbol": "MEW"
-    },
-
-    "BOME": {
-        "type": "dexscreener",
-        "symbol": "BOME"
-    },
-
-    "WBTC": {
-        "type": "dexscreener",
-        "symbol": "WBTC"
-    }
 }
