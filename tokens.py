40d98e9db180aeb1cc074d8f5cf3edc241d26627
Mardog <lukaszsocha1979@gmail.com>
2026-07-22 23:12:46 +0200

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,52 @@
+"""
+Crypto Alerts
+Tokens v1.0
+
+Wszystkie monitorowane tokeny znajdują się w jednym miejscu.
+"""
+
+TOKENS = {
+
+    "ZEUS": {
+        "name": "Zeus Network",
+        "chain": "solana",
+        "kucoin_symbol": None,
+        "mint": ""
+    },
+
+    "PYTH": {
+        "name": "Pyth Network",
+        "chain": "solana",
+        "kucoin_symbol": "PYTH-USDT",
+        "mint": ""
+    },
+
+    "W": {
+        "name": "Wormhole",
+        "chain": "solana",
+        "kucoin_symbol": "W-USDT",
+        "mint": ""
+    },
+
+    "JUP": {
+        "name": "Jupiter",
+        "chain": "solana",
+        "kucoin_symbol": "JUP-USDT",
+        "mint": ""
+    },
+
+    "BOME": {
+        "name": "BOOK OF MEME",
+        "chain": "solana",
+        "kucoin_symbol": "BOME-USDT",
+        "mint": ""
+    },
+
+    "MEW": {
+        "name": "cat in a dogs world",
+        "chain": "solana",
+        "kucoin_symbol": "MEW-USDT",
+        "mint": ""
+    }
+
+}
