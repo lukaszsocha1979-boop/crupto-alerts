6ba3ba88998e39438f2157430d421364a6c15006
Mardog <lukaszsocha1979@gmail.com>
2026-07-22 14:28:41 +0200

    Add files via upload

---
+++ crupto-alerts-main/tokens.py
@@ -0,0 +1,11 @@
+TOKENS = {
+    "BTC": {
+        "type": "coingecko",
+        "id": "bitcoin",
+    },
+
+    "ZEUS": {
+        "type": "solana",
+        "mint": "ZEUS1aR7aX8DFFJf5QjWj2ftDDdNTroMNGo8YoQm3Gq",
+    },
+}
