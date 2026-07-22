c8a81430d3432bbc3a068b3dbf2909dd298a3433
Mardog <lukaszsocha1979@gmail.com>
2026-07-22 14:08:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,16 +43,16 @@ def human(value):
     value = float(value)
 
     if value >= 1_000_000_000_000:
-        return f"{value/1_000_000_000_000:.2f}T"
+        return f"{value / 1_000_000_000_000:.2f}T"
 
     if value >= 1_000_000_000:
-        return f"{value/1_000_000:.2f}B"
+        return f"{value / 1_000_000_000:.2f}B"
 
     if value >= 1_000_000:
-        return f"{value/1_000_000:.2f}M"
+        return f"{value / 1_000_000:.2f}M"
 
     if value >= 1_000:
-        return f"{value/1_000:.2f}K"
+        return f"{value / 1_000:.2f}K"
 
     return f"{value:.2f}"
 
