a15dd752285eb80acf7fbb14ab2ff5d8c6c4c50b
Mardog <lukaszsocha1979@gmail.com>
2026-09-01 09:30:28 +0200

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,9 +1,9 @@
 """
 Crypto Alerts
-Tokens v1.4
+Tokens v1.5
 
 Monitorowane tokeny:
-ZEUS i PYTH
+ZEUS, PYTH, W, JUP, BOME i MEW
 """
 
 TOKENS = {
@@ -18,6 +18,30 @@ TOKENS = {
         "name": "Pyth Network",
         "chain": "solana",
         "mint": "HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3"
+    },
+
+    "W": {
+        "name": "Wormhole",
+        "chain": "solana",
+        "mint": "85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ"
+    },
+
+    "JUP": {
+        "name": "Jupiter",
+        "chain": "solana",
+        "mint": "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"
+    },
+
+    "BOME": {
+        "name": "Book of Meme",
+        "chain": "solana",
+        "mint": "ukHH6c7mMyiWCf1b9pnWe25TSpkDDt3H5pQZgZ74J82"
+    },
+
+    "MEW": {
+        "name": "cat in a dogs world",
+        "chain": "solana",
+        "mint": "MEW1gQWJ3nEXg2qgERiKu7FAFj79PHvQVREQUzScPP5"
     }
 
 }
